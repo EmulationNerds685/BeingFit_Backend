2a2bf1486e9f5c0dc5dd8ed08186158c9a09a834
EmulationNerds685 <bhaskartiwari270@gmail.com>
2025-08-26 12:05:25 +0530

    initial commmit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app=express()
+const port=3000
+app.get('/',(req,res)=>{
+    res.sendStatus(200)
+})
+app.listen(port,()=>{
+    console.log(`Server is listening on http://localhost:${port}`);
+})
