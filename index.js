b593335c0ec94b516607f88614c8208fda95f98f
EmulationNerds685 <bhaskartiwari270@gmail.com>
2025-08-31 12:07:57 +0530

    login and signup page added

--- index.js
+++ index.js
@@ -2,16 +2,42 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors'
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import productRoutes from "./routes/ProductRoute.js";
-
+import userRoutes from "./routes/UserRoute.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // ya jo bhi frontend chal raha hai
+    credentials: true,
+  })
+);
 // Middleware
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "supersecretkey",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+    }),
+    cookie: {
+      httpOnly: true,
+      secure: false, // true rakho agar HTTPS use kar rahe ho
+      maxAge: 1000 * 60 * 60 * 24, // 1 din
+    },
+  })
+);
+
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
