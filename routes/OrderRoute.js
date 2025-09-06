976c9013ad39f93ab195bc765e2f2464f7501867
EmulationNerds685 <bhaskartiwari270@gmail.com>
2025-09-06 11:53:20 +0530

    routes added

--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -3,6 +3,22 @@ import Order from "../models/Order.js";
 
 const router = express.Router();
 
+// ✅ Get all orders
+router.get("/", async (req, res) => {
+  try {
+   const orders = await Order.find()
+  .populate("userId", "name email") // ✅ fetch name & email from User
+  .populate("items.productId", "name price images")
+  .sort({ createdAt: -1 });
+
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+
 // ✅ Create new order (temporary before Razorpay integration)
 router.post("/create", async (req, res) => {
   try {
